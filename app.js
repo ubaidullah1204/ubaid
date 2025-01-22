05e78d0f38ba710227d697960dbcbadee6c69d16
ubaidullah1204 <ubaidkh451@gmail.com>
2025-01-22 11:10:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// var ubaid =  ["u , b ,a , i , d" ]
+// for( var i = ubaid.length; i  < 0  ; i++) {
+//     console.log(ubaid[i])
+// };
+
+// var ubaid = ["1 ", "2", "3", "4", "5"];
+// ubaid.splice( 1 , 3, "8" ,"9")
+// console.log(ubaid)
+
+
+// var ubaid = ["1 ", "2", "3", "4", "5"];
+// console.log( ubaid.slice( 0 , 4 ) );
+// console.log( ubaid.slice( 4 , 5  ) );
+
+
+//  var ubaid = ["1 ", "2", "3", "4", "5"];
+// console.log(ubaid.splice ())
+
+
+// var ubaid = ["1 ", "2", "3", "4", "5"];
+// for ( var i = 0 ; i <ubaid.length ; i++){
+//     console.log(i)
+// };    
+// var  word = prompt("");
+// var palindrome =  '';
+// for ( var i = word.length - 1 ; i >= 0 ; i --){
+//     palindrome += word [i]
+// }
+// if (palindrome === word ){
+//     console.log("its  a palindrome")
+// }else{
+//     console.log("its not a palindrome")
+// }
+
+
+// var str = [ 11 , 12 , 14 ,15 , 20];
+// // var missing = [];
+// for (var i = 0 ; i < str.length ; i++ ){
+// var found =  str [i + 1] -  str[1];
+// if ( found > 1 ){
+//     for ( var j = 1 ; j < found ; j ++ ){
+//         console.log( str [i] + j );
+// }
+// }
+// } console.log(arr)
+
+
+var arr = [ "ubaid" , "ali" , " usman" ];
+arr= prompt("");
+if( arr.length === arr ){
+    console.log("found")
+}else( console.log("not found"))
